2bbc189db151fcbec0ebae90e089d8ed1bee3f46
Na Hyung Choi <schoinh@gmail.com>
2019-06-30 17:21:23 -0700

    save input as a new Date object

--- spec/project-spec.js
+++ spec/project-spec.js
@@ -1,11 +1,24 @@
-// import { XXX } from './../src/backend-code.js';
-//
-// describe('XXX', function() {
-//
-//   it('should ...', function() {
-//     expect(triangle.side1).toEqual(3);
-//     expect(triangle.side2).toEqual(4);
-//     expect(triangle.side3).not.toEqual(6);
-//   });
-// 
-// })
+import { inputDate } from './../src/business-logic';
+
+describe('determine user age', function() {
+  const birthYear = 1991;
+  const birthMonth = 0;
+  const birthDay = 19;
+
+  it('should save input birthday as a Date object', function() {
+    const newDate = new inputDate(birthYear, birthMonth, birthDay);
+    const newBirthday = newDate.makeBirthday(); 
+    expect(newBirthday.getFullYear()).toEqual(1991);
+  });
+
+  // it('should validate date input', function() {
+  //   const invalidYear = 333;
+  //   const invalid
+  //   const invalidDay = 
+
+  //   expect(triangle.side1).toEqual(3);
+  //   expect(triangle.side2).toEqual(4);
+  //   expect(triangle.side3).not.toEqual(6);
+  // });
+
+})
--- src/business-logic.js
+++ src/business-logic.js
@@ -1,1 +1,11 @@
+export class inputDate {
+  constructor(year, month, day) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
 
+  makeBirthday() {
+    return new Date(this.year, this.month, this.day);
+  }
+}
